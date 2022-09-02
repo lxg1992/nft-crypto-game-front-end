0104f6f76eea117a54af72190c2df3e2937faaa9
L G <lxg1992@users.noreply.github.com>
2022-09-02 00:54:06 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import React, { useState, useEffect } from 'react';
+import twitterLogo from './assets/twitter-logo.svg';
+import './App.css';
+
+// Constants
+const TWITTER_HANDLE = 'EthDevCentral';
+const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
+
+const App = () => {
+
+    /*
+   * Just a state variable we use to store our user's public wallet. Don't forget to import useState.
+   */
+  const [currentAccount, setCurrentAccount] = useState(null);
+
+  /*
+   * Start by creating a new action that we will run on component load
+   */
+  // Actions
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { ethereum } = window;
+
+      if (!ethereum) {
+        console.log('Make sure you have MetaMask!');
+        return;
+      } else {
+        console.log('We have the ethereum object', ethereum);
+
+        /*
+         * Check if we're authorized to access the user's wallet
+         */
+        const accounts = await ethereum.request({ method: 'eth_accounts' });
+
+        /*
+         * User can have multiple authorized accounts, we grab the first one if its there!
+         */
+        if (accounts.length !== 0) {
+          const account = accounts[0];
+          console.log('Found an authorized account:', account);
+          setCurrentAccount(account);
+        } else {
+          console.log('No authorized account found');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  /*
+   * This runs our function when the page loads.
+   */
+  useEffect(() => {
+    checkIfWalletIsConnected();
+  }, []);
+
+
+
+  
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="header-container">
+          <p className="header gradient-text">⚔️ Crypto Slayer ⚔️</p>
+          <p className="sub-text">Team up to protect the Metaverse!</p>
+          <div className="connect-wallet-container">
+            <img
+              src="https://64.media.tumblr.com/tumblr_mbia5vdmRd1r1mkubo1_500.gifv"
+              alt="Monty Python Gif"
+            />
+          </div>
+        </div>
+        <div className="footer-container">
+          <img alt="Twitter Logo" className="twitter-logo" src={twitterLogo} />
+          <a
+            className="footer-text"
+            href={TWITTER_LINK}
+            target="_blank"
+            rel="noreferrer"
+          >{`built by @${TWITTER_HANDLE}`}</a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
